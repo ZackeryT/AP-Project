9935a6a2c73b34f20512134ca27ce6d8fef9b587
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2022-09-19 12:30:01 -0400

    fgregerg

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const alphabet = ["A","B","C",
+var alphabet = ["A","B","C",
 "D","E","F","G","H","I","J","K",
 "L","M","N","O","P","Q","R","S","T",
 "U","V","W","X","Y","Z"];
 const URL = "https://random-word-api.herokuapp.com/word";
 
+function printBtn(c){
+    for (var i = 0; i < c.length; i++){
+        $('.letters').append('<button value='+ c[i] + '>' + '</button>'))
+    }
+}
+printBtn(alphabet);
+
 async function getWord(URL){
     const response = await fetch(URL);
     console.log(response);
 }
-getWord();
+getWord.type(URL);
