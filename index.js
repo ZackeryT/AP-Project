cecd6cce6a3f40531042d4fbfdc5be4b4229b087
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2022-09-23 12:33:56 -0400

    some stuff good luck zack

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ var alphabet = ["A","B","C",
 "D","E","F","G","H","I","J","K",
 "L","M","N","O","P","Q","R","S","T",
 "U","V","W","X","Y","Z"];
-const URL = "https://random-word-api.herokuapp.com/word";
+const URL = "https://random-word-api.herokuapp.com/all";
+const api = await fetch(URL);
+const wordlist = api.json;
 
+console.log(wordlist);
 function printBtn(){
     for (i = 0; i < alphabet.length; i++){
   document.getElementById('letter').innerHTML +=
    '<button id="' + alphabet[i] + '"onclick="getLetter()">'  + alphabet[i] + '</button>';
  }
 }
+function take(){
+  const lives = 6;
+  document.getElementById('lives').innerHTML
+
+}
 function getLetter(){
     console.log();
-}
+  }
+
+
 printBtn();
