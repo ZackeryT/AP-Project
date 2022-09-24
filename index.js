a3ca46c34c979137ad67a593daf91b3a459a381c
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2022-09-24 11:44:08 -0500

    obtained word we guess tmrw

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
-var alphabet = ["A","B","C",
-"D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S","T",
-"U","V","W","X","Y","Z"];
+var alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 const URL = "https://random-word-api.herokuapp.com/all";
-const api = await fetch(URL);
-const wordlist = api.json;
-
-console.log(wordlist);
-function printBtn(){
-    for (i = 0; i < alphabet.length; i++){
-  document.getElementById('letter').innerHTML +=
-   '<button id="' + alphabet[i] + '"onclick="getLetter()">'  + alphabet[i] + '</button>';
- }
+async function getWord() {
+  const api = await fetch(URL).then((api) => api.json());
+  const word = api[Math.floor(Math.random() * api.length)];
+  window.localStorage.setItem("word", word);
+  console.log(word);
 }
-function take(){
-  const lives = 6;
-  document.getElementById('lives').innerHTML
+getWord();
+const word = window.localStorage.getItem("word");
 
-}
-function getLetter(){
-    console.log();
+function printBtn() {
+  for (i = 0; i < alphabet.length; i++) {
+    document.getElementById("letter").innerHTML +=
+      '<button id="' +
+      alphabet[i] +
+      '"onclick="getLetter()">' +
+      alphabet[i] +
+      "</button>";
   }
-
+}
+function take() {
+  const lives = 6;
+  document.getElementById("lives").innerHTML;
+}
+function getLetter() {
+  console.log();
+}
 
 printBtn();
