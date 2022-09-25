39c09c31203736a890a94a41a338b364d3295f08
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2022-09-25 14:00:29 -0400

    did a bit

--- index.js
+++ index.js
@@ -26,32 +26,52 @@ var alphabet = [
   "Y",
   "Z",
 ];
+var lives = document.getElementById("lives");
+const storedguess = [];
 const URL = "https://random-word-api.herokuapp.com/all";
+
 async function getWord() {
   const api = await fetch(URL).then((api) => api.json());
   const word = api[Math.floor(Math.random() * api.length)];
   window.localStorage.setItem("word", word);
-  console.log(word);
+  console.log("done");
 }
+console.log(window.localStorage.getItem("word"));
 getWord();
-const word = window.localStorage.getItem("word");
 
 function printBtn() {
   for (i = 0; i < alphabet.length; i++) {
     document.getElementById("letter").innerHTML +=
-      '<button id="' +
-      alphabet[i] +
-      '"onclick="getLetter()">' +
-      alphabet[i] +
-      "</button>";
+      "<button onclick=check()>" + alphabet[i] + "</button>";
   }
 }
-function take() {
-  const lives = 6;
-  document.getElementById("lives").innerHTML;
-}
-function getLetter() {
-  console.log();
-}
+guessed = function () {
+  guess = document.getElementById("guess");
+  dashes = document.createElement("ul");
+  word = window.localStorage.getItem("word");
+  for (var i = 0; i < word.length; i++) {
+    dashes.setAttribute("id", "correctWord");
+    guesses = document.createElement("li");
+    guesses.setAttribute("class", "guess");
+    guesses.innerHTML = "_";
 
+    storedguess.push(guesses);
+    guess.appendChild(dashes);
+    dashes.appendChild(guesses);
+  }
+};
+check = function () {
+  var guessedLetter = this.innerHTML;
+  this.setAttribute("class", "active");
+  this.onclick = null;
+  var word = window.localStorage.getItem("word");
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === guessedLetter) {
+      storedguess[i].innerHTML = guessedLetter;
+      counter += 1;
+    }
+  }
+};
+
+guessed();
 printBtn();
