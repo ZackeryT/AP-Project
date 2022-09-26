57ea6baedb0dfe6438d9f17588f657a7160f06a9
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2022-09-26 12:44:03 -0400

    HANGMAN YEA BABY

--- index.js
+++ index.js
@@ -26,52 +26,63 @@ var alphabet = [
   "Y",
   "Z",
 ];
+var storedguess = [];
 var lives = document.getElementById("lives");
-const storedguess = [];
-const URL = "https://random-word-api.herokuapp.com/all";
+const URL = "https://random-word-api.herokuapp.com/word";
 
-async function getWord() {
-  const api = await fetch(URL).then((api) => api.json());
-  const word = api[Math.floor(Math.random() * api.length)];
-  window.localStorage.setItem("word", word);
-  console.log("done");
-}
-console.log(window.localStorage.getItem("word"));
-getWord();
-
-function printBtn() {
-  for (i = 0; i < alphabet.length; i++) {
-    document.getElementById("letter").innerHTML +=
-      "<button onclick=check()>" + alphabet[i] + "</button>";
-  }
-}
 guessed = function () {
-  guess = document.getElementById("guess");
-  dashes = document.createElement("ul");
+  holder = document.getElementById("guess");
+  correct = document.createElement("ul");
   word = window.localStorage.getItem("word");
+
   for (var i = 0; i < word.length; i++) {
-    dashes.setAttribute("id", "correctWord");
+    correct.setAttribute("id", "correctWord");
     guesses = document.createElement("li");
     guesses.setAttribute("class", "guess");
     guesses.innerHTML = "_";
 
     storedguess.push(guesses);
-    guess.appendChild(dashes);
-    dashes.appendChild(guesses);
+    holder.appendChild(correct);
+    correct.appendChild(guesses);
   }
 };
-check = function () {
-  var guessedLetter = this.innerHTML;
-  this.setAttribute("class", "active");
-  this.onclick = null;
+async function getWord() {
+  const api = await fetch(URL).then((api) => api.json());
+  const word1 = api[Math.floor(Math.random() * api.length)];
+  const word = word1.toUpperCase();
+  window.localStorage.setItem("word", word);
+  console.log("done");
+  guessed();
+  console.log(window.localStorage.getItem("word"));
+}
+
+getWord();
+
+function printBtn() {
+  for (i = 0; i < alphabet.length; i++) {
+    document.getElementById("letter").innerHTML +=
+      '<button id="' +
+      alphabet[i] +
+      '" onclick="onClick(this.innerHTML)">' +
+      alphabet[i] +
+      "</button>";
+  }
+}
+
+function onClick(clicked_id) {
+  console.log(clicked_id);
+  var geuss = clicked_id;
   var word = window.localStorage.getItem("word");
+  console.log(storedguess);
   for (var i = 0; i < word.length; i++) {
-    if (word[i] === guessedLetter) {
-      storedguess[i].innerHTML = guessedLetter;
-      counter += 1;
+    if (word[i] === geuss) {
+      storedguess[i].innerHTML = geuss;
+    }
+    var j = word.indexOf(geuss);
+    if (j === -1) {
+      console.log("not there");
     }
   }
-};
+}
 
-guessed();
 printBtn();
